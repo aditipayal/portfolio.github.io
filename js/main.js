14dab5b98c32b6ba62c5651d9609ce64ff83e888
Aditi <aditipayalagarwal@gmail.com>
2021-05-04 17:41:39 +0530

    Front Changes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
- $('.circlechart').circlechart();
-// $(".progress-bar").loading();
+on('click', '.mobile-nav-toggle', function(e) {
+    select('body').classList.toggle('mobile-nav-active')
+    this.classList.toggle('navigation-menu')
+    this.classList.toggle('icofont-x')
+  })
