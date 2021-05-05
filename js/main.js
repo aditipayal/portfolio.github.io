6dab57acc8022e8121769800eeeda3ba233a5e17
Aditi <aditipayalagarwal@gmail.com>
2021-05-05 15:15:13 +0530

    Changes in Mobile view

--- js/main.js
+++
@@ -1,5 +0,0 @@
-on('click', '.mobile-nav-toggle', function(e) {
-    select('body').classList.toggle('mobile-nav-active')
-    this.classList.toggle('navigation-menu')
-    this.classList.toggle('icofont-x')
-  })
